8da5b8917b75aa183c26ef25b003f7b4e39bbe41
Sergey Kucher <sergey.kucher@gmail.com>
2020-05-28 15:41:21 +0300

    remove ASG and chahge LC

--- files/app_user_data.sh
+++ files/app_user_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo aws s3 sync s3://footgo/war /home/ec2-user
-chmod 755 /home/ec2-user/ROOT.war
+sudo aws s3 sync s3://footgo/target /home/ec2-user
+sudo chmod 755 /home/ec2-user/ROOT.war
 sudo /home/ec2-user/ROOT.war > /dev/null 2>&1&
